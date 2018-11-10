0f2ce78b1efb62f4213e780aa7e4cafb57027432
Christian Barral <44912626+cblopez@users.noreply.github.com>
2018-11-10 02:37:34 +0100

    mvcPHPGenerator 1.1
    Added index.php generation

--- mvcPHPGenerator.py
+++ mvcPHPGenerator.py
@@ -105,7 +105,7 @@ def appendToGatherData(entityName, attributesList):
     gather.write('function gatherData' + entityName + '(){\r\n')
     gather.write('\r\n')
     for attribute in attributesList:
-        gather.write('\t$' + attribute + ' = \'\'\r\n')
+        gather.write('\t$' + attribute + ' = \'\';\r\n')
     gather.write('\r\n')
     gather.write('\tif($_POST){\r\n')
     gather.write('\r\n')
@@ -148,6 +148,48 @@ def closeGatherData():
     gather.write('?>')
 
     gather.close()
+
+def createIndex():
+    '''
+    Creates an index.php on the root directory of the output folder
+    Note: the index.php will be used as a common entry point
+    '''
+    index = open('./MVCPHPGenerator/index.php', 'w')
+
+    index.write('<?php\r\n')
+    index.write('\r\n')
+    index.write('define(\"DEFAULT_CONTROLLER\", \"Index\");\r\n')
+    index.write('define(\"DEFAULT_ACTION\", \"index\");\r\n')
+    index.write('\r\n')
+    index.write('\r\n')
+    index.write('function run(){\r\n')
+    index.write('\ttry{\r\n')
+    index.write('\t\tif(!isset($_GET[\"controller\"])){\r\n')
+    index.write('\t\t\t$_GET[\"controller\"] = DEFAULT_CONTROLLER;\r\n')
+    index.write('\t\t}\r\n')
+    index.write('\t\tif(!isset($_GET[\"action\"])){\r\n')
+    index.write('\t\t\t$_GET[\"action\"] = DEFAULT_ACTION;\r\n')
+    index.write('\t\t}\r\n')
+    index.write('\r\n')
+    index.write('\t\t$controller = loadController($_GET[\"controller\"]);\r\n')
+    index.write('\r\n')
+    index.write('\t\tinclude (string)$controller;\r\n')
+    index.write('\r\n')
+    index.write('\t} catch(Exception $ex){\r\n')
+    index.write('\t\tdie(\"An exception has occured: \" . $ex->getMessage());\r\n')
+    index.write('\t}\r\n')
+    index.write('}\r\n')
+    index.write('\r\n')
+    index.write('function loadController($controllerName){\r\n')
+    index.write('\t$controllerClassName = __DIR__.\"/Controllers/\".$controllerName.\"_Controller.php\";\r\n')
+    index.write('\treturn $controllerClassName;\r\n')
+    index.write('}\r\n')
+    index.write('\r\n')
+    index.write('run();\r\n')
+    index.write('?>\r\n');
+
+    index.close();
+
 
 
 def main():
@@ -201,6 +243,8 @@ def main():
         option = raw_input("Would you like to add more entities? (yes/no): ")
 
     closeGatherData()
+    print("[+] Creating index.php")
+    createIndex()
     print("[+] Finished successfully.")
     print("IMPORTANT: Before using the project, please head to MVCPHPGenerator/Functions/gatherData.php and set the models includes" +
     " at the beginning of the script, where it says \'//Fill with includes\'")
