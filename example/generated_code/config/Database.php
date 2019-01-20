3d86d2de2261d347d3396dbd1035cd0d590f0aa9
Christian Barral <44912626+cblopez@users.noreply.github.com>
2019-01-20 18:17:09 +0100

    Example initial commit

---
+++ example/generated_code/config/Database.php
@@ -0,0 +1,27 @@
+<?php
+
+class Database{
+
+	//Database attributes
+	private $host = 'localhost';
+	private $db_name = 'test';
+	private $username = 'test';
+	private $password = 'test';
+	private $conn;
+
+	//Database connection
+	public function connect(){
+
+		$this->conn = null;
+
+		try{
+			$this->conn = new PDO('mysql:host=' . $this->host . 'dbname=' . $this->db_name, $this->username, $this->password);
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch (PDOException $e){
+			die('Connection Error: ' . $e->getMessage());
+		}
+
+		return $this->conn;
+
+	}
+}
